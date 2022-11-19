b482691c3f93a666f303055bba6f1411c9bdefc0
CUONG <cuongb1910498@gmail.com>
2022-11-19 21:43:09 +0700

    chinh sua trang tim kiem

--- src/modules/timkiem.php
+++ src/modules/timkiem.php
@@ -6,17 +6,19 @@
     );
     $stmt->execute([
         'tenviec' => '%'.$key.'%',
-        'tt' =>1
+        'tt' => 1
     ]);
     $rows = $stmt->fetchAll();
     $i = 0;
-    foreach ($rows as $row){
-        $i++;
 ?>
 
 <div class="row offset-lg-1 offset-2" id="hienthi">
 
-    
+    <?php
+        if($key != ''){
+        foreach ($rows as $row){
+            $i++;
+    ?>
     <div class="card" style="width: 15rem;" class="col-lg-3 col-md-4 col-sm-4 col-12" 
         data-bs-toggle="tooltip" data-bs-placement="top" title="<?php echo $row['tenviec'] ?>">
         <?php
@@ -37,24 +39,17 @@
 
             <div class="card-body">
             <h5 class="card-title"><?php echo substr($row['tenviec'], 0, 30) ?></h5>
-            <?php
-                $id = $row['id'];
-                $query = $pdo->prepare(
-                    "SELECT mota FROM vieclam WHERE id = :id"
-                );
-                $query->execute(['id' => $id]);
-                $mota = $query->fetch();
-                
-            ?>
-            <p class="card-text"><?php echo substr($mota['mota'], 0, 50).'......' ?></p>
+            <p class="card-text"><?php echo substr($row['mota'], 0, 50).'......' ?></p>
             <a href="index.php?quanly=chitietbai&id=<?php echo $row['id'] ?>" class="btn btn-primary stretched-link">Xem chi tiết</a>
         </div>
     </div>
           
     <?php
+        }if($i==0) echo "<h1>không có kết quả tìm kiếm cho: '$key'</h1>";
+    
+    }else {
+        echo "<h1>không có kết quả tìm kiếm cho: '$key'</h1>";
     }
-        if($i == 0)
-            echo "<h1>không có kết quả tìm kiếm cho: '$key'</h1>";
     ?>
             
 </div>
